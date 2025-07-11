3b2bcf0a94f5a614864d06a6818c5a70f4760d19
Sagar.koju <sagar.koju@mypay.com.np>
2025-07-11 15:24:22 +0545

    deploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1161967878"
+    serviceWorkerVersion: "1150618776"
   }
 });
